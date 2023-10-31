25d9d6b5476dc9d44a3ef9848aef558186b4333b
Guille <g.mirandagaleote@gmail.com>
2023-10-31 09:31:03 +0100

    Finish

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,4 +1,18 @@
-export const Counter = () => {
-  const count = 0;
-  retunr(
-    
+
+type Props = {
+  title: string;
+ };
+ export function Info ({title}: Props){
+   title = ' The pointing gentlemen'
+  return(
+   <div>
+    <header className="main-header">
+      <h1 className="main-title">{title}</h1>
+    </header>
+      <section className="controls">
+        <p className="info">0 gentlemen pointing at you</p>
+        <button className="button button--select">Select all</button>
+      </section>
+    </div>
+   );
+ }
